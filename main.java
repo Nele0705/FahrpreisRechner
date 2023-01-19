31449dab1131ee8d90524cccf3193bfbad0724bf
Nele0705 <78144286+Nele0705@users.noreply.github.com>
2023-01-19 11:33:10 +0100

    Create main.java

---
+++ main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Geben Sie ein wie viele Minuten Sie fahren");
+		Scanner scanner = new Scanner(System.in);
+		int inputMinuten = scanner.nextInt();
+		
+		int grundpreis = 2; 
+		double variablerPreis = 0.25; 
+		
+		double endpreis = inputMinuten * variablerPreis + grundpreis; 
+		
+		System.out.println("Ihr Endpreis ist: ");
+
+		System.out.println(endpreis + "€");
+		
+	}
+}
